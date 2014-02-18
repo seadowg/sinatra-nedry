ec1a3a7882c15f8d513ddea866cd67805ef731d1
Callum Stott <callum@seadowg.com>
2014-02-18 23:34:50 +0000

    Initial commit

---
+++ lib/sinatra/nedry.rb
@@ -0,0 +1,52 @@
+module Sinatra
+  module Nedry
+    def protected!(&blk)
+      if authorized?
+        blk.call
+      else
+        not_authorized
+      end
+    end
+    
+    def flagged!(&blk)
+      if authorized? || ENV["RACK_ENV"] != "production"
+        blk.call
+      else
+        not_authorized
+      end
+    end
+    
+    private
+    
+    def authorized?
+      admin_credentials = if ENV["RACK_ENV"] == "production"
+        [ENV["ADMIN_USERNAME"], ENV["ADMIN_PASSWORD"]]
+      else
+        ["admin", "admin"]
+      end      
+  
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == admin_credentials
+    end
+    
+    def not_authorized
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, NOT_AUTH_TEMPLAT
+    end
+    
+    NOT_AUTH_TEMPLATE = <<-ERB
+      <div style="text-align: center; margin-top: 40px">  
+        <iframe 
+          width="853" 
+          height="480" 
+          src="//www.youtube.com/embed/RfiQYRn7fBg?autoplay=1&loop=1&playlist=RfiQYRn7fBg" 
+          frameborder="0" 
+          allowfullscreen>
+        </iframe>
+        <h1>Ah ah ah!</h1>
+      </div>
+    ERB
+  end
+
+  helpers Nedry
+end
---
+++ sinatra-nedry.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = "sinatra-nedry"
+  s.version = "0.0.1"
+
+  s.authors = ["Callum Stott"]
+  s.email = ["callum@seadowg.com"]
+  s.summary = "HTTP Basic Auth helpers for Sinatra with built in Ah ah ah!"
+  
+  s.files = ["lib/sinatra/nedry.rb"]
+  s.require_paths = ["lib"]
+  
+  s.add_dependency "sinatra"
+end
